92c4743ba6109f8e5ff92475137eee5816274275
xjcooo <519877142@qq.com>
2018-11-29 22:49:33 +0800

    add customer

--- demo_web/src/main/java/org/xjc/demo/configs/AmqpConfigs.java
+++ demo_web/src/main/java/org/xjc/demo/configs/AmqpConfigs.java
@@ -1,4 +1,15 @@
 package org.xjc.demo.configs;
 
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AmqpConfigs {
+
+    @Bean
+    public Queue getQueue(){
+        return new Queue("spring.boot.amqp.xjc.test.workQueues", true, false, false);
+    }
+
 }
--- demo_web/src/main/java/org/xjc/demo/customer/QueueReceiver.java
+++ demo_web/src/main/java/org/xjc/demo/customer/QueueReceiver.java
@@ -1,4 +1,18 @@
 package org.xjc.demo.customer;
 
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class QueueReceiver {
+
+    @Autowired
+    private AmqpTemplate amqpTemplate;
+
+    @RabbitListener(queues = "spring.boot.amqp.xjc.test.workQueues")
+    public void receiveMessage(String person){
+        System.out.println("message:<"+person+">");
+    }
 }
