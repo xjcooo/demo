41dbcb0cba85c7c6b69bf1e80b1f7a3e4c54cd39
xjcooo <xujiancheng@touker.com>
2019-05-16 09:15:06 +0800

    feat(redis-cluster): 集群和单机切换

--- demo_redis_cluster/src/test/java/org/xjc/demo/redis/OperationsTest.java
+++ demo_redis_cluster/src/test/java/org/xjc/demo/redis/OperationsTest.java
@@ -19,7 +19,7 @@ public class OperationsTest {
     private JedisCluster jc;
 
     @Before
-    public void init(){
+    public void init() {
         Set<HostAndPort> jedisClusterNodes = new HashSet<HostAndPort>();
         //Jedis Cluster will attempt to discover cluster nodes automatically
         jedisClusterNodes.add(new HostAndPort("10.0.30.62", 7000));
@@ -32,7 +32,7 @@ public class OperationsTest {
     }
 
     @Test
-    public void test(){
+    public void test() {
         jc.set("foo", "bar");
         String value = jc.get("foo");
         System.out.println(value);
@@ -53,12 +53,13 @@ public class OperationsTest {
 
 
         List<String> fields = new ArrayList<String>();
-        for (int i = 0; i < 10000; i++){
-            redis.hset(key, field + i, value + i );
-            fields.add(field+i);
+        for (int i = 0; i < 10000; i++) {
+            redis.hset(key, field + i, value + i);
+            fields.add(field + i);
         }
-        System.out.println(redis.hget(key, field+0));
-        String[] ss = new String[10000];fields.toArray(ss);
+        System.out.println(redis.hget(key, field + 0));
+        String[] ss = new String[10000];
+        fields.toArray(ss);
         System.out.println(redis.hmget(key, ss));
         redis.delete(key);
 
@@ -66,14 +67,19 @@ public class OperationsTest {
         System.out.println("spendTime : " + stopWatch.getTotalTimeMillis());
     }
 
+    String singleNode = "10.0.30.114:6379";
+    String clusterNode = "10.0.30.114:7000,10.0.30.62:7000,10.0.30.183:7000";
+
+    boolean isCluster = true;
+
     @Test
     public void testOperate2() throws Exception {
         StopWatch stopWatch = new StopWatch();
         stopWatch.start();
         String key = "key", field = "feild7", value = "WarnConditionStrategy{WarnStrategy{warnId=133762, userId=9, stockName=平安银行, stockCode=000001, securityType=4, exchange=SZ, warnValue=0.00, strategyType=9, strategyState=1, isDelete=0, triggerCount=0, availableTimeBegin=Thu Jan 01 09:30:00 CST 1970, availableTimeEnd=Thu Jan 01 15:00:00 CST 1970, businessType=2, testFlag=0}, customerNo=130082, trackType=4, trackCode=000001, trackName=平安银行, trackExchange=SZ, weekBegin=1, weekEnd=5, deviationCtrlParam=DeviationCtrlEnabledParam{option=NON_ORDER, limit=2.00}, dataVersion=2, strategyPropertiesObj=GridStrategyProperties{upperLimitPrice=15.00, lowerLimitPrice=10.00, buyQuantity=100, sellQuantity=100, increaseSeqType=0, decreaseSeqType=0, increase=0.50, decrease=-0.50, reactionRatio=-0.04, rallyRatio=0.01, buyStrategy=6, sellStrategy=7, aboveUpperPriceSettings=0, belowLimitPriceSettings=0, multipliedEntrust=1, brokenPriceTriggerOption=1}, dynamicPropertiesObj=GridDynamicProperties{basePrice=12.81, isBuySignal=false, isSellSignal=false}, confirmCount=0, confirmDelayType=2, confirmDelayTimes=5, entrustMethod=0, orderType=0, exchangeMethod=2, nodeValue=TJD;iPhone 6;;13800000009;, openness=1}";
-        Operation redis = new Operation("10.0.30.114:6379");
         Map<String, String> map = new HashMap<String, String>();
-        for (int i = 0; i < 10000; i++){
+        Operation redis = new Operation(isCluster ? clusterNode : singleNode);
+        for (int i = 0; i < 10000; i++) {
             map.put(field + i, value + i);
         }
         redis.hmset(key, map);
