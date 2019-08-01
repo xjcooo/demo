9b99c3fb459a2eff7e2cd68c595d7bc59b7ce956
许建程 <xujiancheng@touker.com>
2019-08-01 19:30:59 +0800

    feat(module): springSecurity自定义配置方式

---
+++ demo2_security/src/main/java/org/xjc/demo/SecurityApplication.java
@@ -0,0 +1,16 @@
+package org.xjc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by xjc on 2019-8-1.
+ */
+@SpringBootApplication
+public class SecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
+
+}
