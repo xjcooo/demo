9f357c5c290230a76661aaa5109acc09f8ffab63
许建程 <xujiancheng@touker.com>
2019-08-08 17:01:41 +0800

    feat(module): springSecurity自建UserDetailsService

--- demo2_security/src/main/java/org/xjc/demo/config/SpringSecurityWebConfig.java
+++ demo2_security/src/main/java/org/xjc/demo/config/SpringSecurityWebConfig.java
@@ -1,11 +1,17 @@
 package org.xjc.demo.config;
 
+import com.google.common.collect.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
@@ -37,10 +43,15 @@ public class SpringSecurityWebConfig extends WebSecurityConfigurerAdapter {
     /*使用@Autowired方式, 可以适用跨越多个WebSecurityConfigurerAdapter的情况,*/
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder())
-                .withUser("user")
-                .password(new BCryptPasswordEncoder().encode("user"))
-                .roles("USER");
+        auth.userDetailsService(new UserDetailsService() {
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                return new User("user", new BCryptPasswordEncoder().encode("user"), Lists.<GrantedAuthority>newArrayList());
+            }
+        }).passwordEncoder(new BCryptPasswordEncoder());
+//        auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+//                .withUser("user")
+//                .password(new BCryptPasswordEncoder().encode("user"))
+//                .roles("USER");
     }
 
 }
