3d2eeb90deeded9bfb44b6366c3774ebc6072099
许建程 <wb_xujc@zts.com.cn>
2020-01-22 18:32:09 +0800

    feat(module): 主页添加测试模块

--- demo_vue/src/components/FeatureTest.vue
+++ demo_vue/src/components/FeatureTest.vue
@@ -1,29 +1,40 @@
 <template>
-  <div id="app1">
-    <h1>{{ title }}</h1>
-    <p>链接：<a v-bind:href="urlBiDu">百度</a></p>
-    <p>按钮点击次数：{{counter}}次</p>
-    <button v-on:click="counter++">Add 1</button>
-  </div>
+    <div id="app1">
+        <h1>{{ title }}</h1>
+        <p>链接：<a v-bind:href="urlBiDu">百度</a></p>
+        <p>按钮点击次数：{{counter}}次</p>
+        <button v-on:click="counter++">Add 1</button>
+        <p>{{message}}</p>
+    </div>
 </template>
 
 <script>
-export default {
-  name: "FeatureTest",
-  props: {
-    title: String
-  },
-  data() {
-    return {
-      urlBiDu: "http://www.baidu.com",
-      counter: 0
-    }
-  }
-};
+    export default {
+        name: "FeatureTest",
+        props: {
+            title: String
+        },
+        data() {
+            return {
+                urlBiDu: "http://www.baidu.com",
+                counter: 0,
+                message: ""
+            }
+        },
+        mounted() {
+            this.$axios.get('http://localhost:8080/api', {
+                params: {}
+            }).then(response => {
+                this.message = "response:" + response.data;
+            }).catch(error => {
+                this.message = "error:" + error.message;
+            });
+        }
+    };
 </script>
 
 <style scoped>
-#app1 {
-  width: 100%;
-}
+    #app1 {
+        width: 100%;
+    }
 </style>
