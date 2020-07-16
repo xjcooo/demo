0ac09b1a9cde9fd40178ba909661d520655b8e39
许建程 <wb_xujc@zts.com.cn>
2020-07-16 13:37:57 +0800

    feat(module): demo2重构

--- demo2_parent/demo2_web/src/main/java/org/xjc/demo/controller/DemoController.java
+++ demo2_parent/demo2_web/src/main/java/org/xjc/demo/controller/DemoController.java
@@ -5,19 +5,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
- * Created by xjc on 2019-7-19.
+ *
+ * @author xjc
+ * @date 2019-7-19
  */
 @Controller
 @RequestMapping("demo")
 public class DemoController {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String home() throws Exception {
+    public String home() {
         return "index";
     }
 
     @RequestMapping(value = "index", method = RequestMethod.GET)
-    public String index() throws Exception {
+    public String index() {
         return "index";
     }
 
